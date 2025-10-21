6b40eeafc34343a500e08904a788dd9d5c8c96ba
Foxhound-PC\Foxhound <andwamb@gmail.com>
2025-10-21 23:11:09 +0200

    Arrow have better selection fit now

--- src/items/shapes/lines.py
+++ src/items/shapes/lines.py
@@ -214,26 +214,63 @@ class LineItem(HandleAwareItemMixin, QtWidgets.QGraphicsPathItem):
         return rect.adjusted(-padding, -padding, padding, padding)
 
     def shape(self) -> QtGui.QPainterPath:  # type: ignore[override]
+        points = self._points
+
+        pen = self.pen()
+        pen_width = max(pen.widthF(), 0.1)
+        half_width = pen_width / 2.0 + self._selection_padding
+
         base_path = QtGui.QPainterPath(self.path())
+
+        selection_path = QtGui.QPainterPath()
+        selection_path.setFillRule(QtCore.Qt.FillRule.OddEvenFill)
+
+        if half_width > 0.0 and len(points) >= 2:
+            for start_point, end_point in zip(points, points[1:]):
+                direction = end_point - start_point
+                length = math.hypot(direction.x(), direction.y())
+                if length <= 1e-6:
+                    continue
+
+                unit_dir = QtCore.QPointF(direction.x() / length, direction.y() / length)
+                perp = QtCore.QPointF(-unit_dir.y(), unit_dir.x())
+
+                extension = QtCore.QPointF(unit_dir.x() * half_width, unit_dir.y() * half_width)
+                offset = QtCore.QPointF(perp.x() * half_width, perp.y() * half_width)
+
+                rect_polygon = QtGui.QPolygonF(
+                    [
+                        start_point - extension + offset,
+                        start_point - extension - offset,
+                        end_point + extension - offset,
+                        end_point + extension + offset,
+                    ]
+                )
+                segment_path = QtGui.QPainterPath()
+                segment_path.addPolygon(rect_polygon)
+                selection_path.addPath(segment_path)
+
         if self.arrow_start or self.arrow_end:
-            points = self._points
             if len(points) >= 2:
+                arrow_path = QtGui.QPainterPath()
                 if self.arrow_start:
                     start_poly, _ = self._arrow_head_geometry(points[1], points[0])
-                    base_path.addPolygon(start_poly)
+                    arrow_path.addPolygon(start_poly)
                 if self.arrow_end:
                     end_poly, _ = self._arrow_head_geometry(points[-2], points[-1])
-                    base_path.addPolygon(end_poly)
-        pen = self.pen()
-        stroker = QtGui.QPainterPathStroker()
-        stroker.setCapStyle(pen.capStyle())
-        stroker.setJoinStyle(pen.joinStyle())
-        stroker.setMiterLimit(4.0)
-        width = max(pen.widthF(), 0.1) + self._selection_padding * 2.0
-        stroker.setWidth(width)
-        stroked = stroker.createStroke(base_path)
-        stroked.addPath(base_path)
-        return stroked
+                    arrow_path.addPolygon(end_poly)
+
+                if not arrow_path.isEmpty():
+                    selection_path.addPath(arrow_path)
+                    if self._selection_padding > 0.0:
+                        arrow_stroke = QtGui.QPainterPathStroker()
+                        arrow_stroke.setCapStyle(QtCore.Qt.PenCapStyle.SquareCap)
+                        arrow_stroke.setJoinStyle(QtCore.Qt.PenJoinStyle.MiterJoin)
+                        arrow_stroke.setMiterLimit(4.0)
+                        arrow_stroke.setWidth(self._selection_padding * 2.0)
+                        selection_path.addPath(arrow_stroke.createStroke(arrow_path))
+
+        return selection_path
 
     def update_handles(self) -> None:
         while len(self._handles) < len(self._points):
@@ -341,9 +378,19 @@ class LineItem(HandleAwareItemMixin, QtWidgets.QGraphicsPathItem):
             painter.setBrush(self.pen().color())
             for polygon in arrow_polygons:
                 painter.drawPolygon(polygon)
-            painter.restore()
+        painter.restore()
 
         if _should_draw_selection(self):
+            highlight_path = self.shape()
+            if not highlight_path.isEmpty():
+                painter.save()
+                highlight_color = QtGui.QColor(255, 235, 59)
+                highlight_color.setAlpha(80)
+                painter.setPen(QtGui.QPen(QtCore.Qt.PenStyle.NoPen))
+                painter.setBrush(highlight_color)
+                painter.drawPath(highlight_path)
+                painter.restore()
+
             painter.save()
             selection_pen = QtGui.QPen(PEN_SELECTED)
             selection_pen.setCapStyle(self.pen().capStyle())
